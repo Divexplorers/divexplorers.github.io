f747f4c93bd20bf5904b7e3f9dd8ba339630ecc0
Arivazhagan <arivu86@gmail.com>
2014-06-21 19:44:01 +0530

    Intial Template

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+ $(document).ready(function() {
+    //$("#contact").modal();
+    $.fn.fullpage({
+    	slidesColor: ['#FFFFFF', '#f25764', '#66b76d', '#000000', '#fcc349'],
+		anchors: ['Home', 'Expert', 'MyStuff','Blink', 'Contact'],
+		menu: '#top_nav'});
+  });
